1df3a5acc353105692f8529bd8f677159668c6df
vondry66 <vondry66@gmail.com>
2022-09-05 14:59:55 +0100

    removes app.use(express.json) as requested

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require("express");
 const { getCategories } = require("./controllers/categories.controller");
 const app = express();
 
-app.use(express.json());
-
 app.get("/api/categories", getCategories);
 
 app.use((err, req, res, next) => {
