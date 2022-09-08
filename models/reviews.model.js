8332ae3f7056778b7c6e2fa05ded941e8e37d95f
vondry66 <vondry66@gmail.com>
2022-09-08 15:54:30 +0100

    adds sorted number 7

--- models/reviews.model.js
+++ models/reviews.model.js
@@ -1,8 +1,34 @@
 const db = require("../db/connection");
-const selectReviews = (review_id) => {
+const selectReviewsById = (review_id) => {
   return db
-    .query(`SELECT * FROM reviews WHERE review_id=$1;`, [review_id])
+    .query(
+      `SELECT
+    reviews.review_id,
+    reviews.title,
+    reviews.category,
+    reviews.designer,
+    reviews.owner,
+    reviews.review_body,
+    reviews.review_img_url,
+    reviews.created_at,
+    reviews.votes,
+    COUNT (comments.review_id):: INT AS comment_count
+    FROM reviews
+    LEFT JOIN comments ON reviews.review_id = comments.review_id
+    WHERE reviews.review_id=$1
+    GROUP BY reviews.review_id
+    `,
+      [review_id]
+    )
     .then((result) => {
+      console.log(result.rows);
+      const reviewID = result.rows[0];
+      if (!reviewID) {
+        return Promise.reject({
+          status: 404,
+          msg: `review ${review_id} doesn't exist`,
+        });
+      }
       return result.rows[0];
     });
 };
@@ -27,4 +53,41 @@ const updateReviewsById = (review_id, updateVotes) => {
       return result.rows[0];
     });
 };
-module.exports = { selectReviews, updateReviewsById };
+
+const selectReviews = (sort_by = "category", order = "desc", category) => {
+  const validColumns = ["category", "created_at"];
+  if (!validColumns.includes(sort_by)) {
+    return Promise.reject({ status: 400, msg: "Bad request !" });
+  }
+  let queryStr = `SELECT
+    reviews.review_id,
+    reviews.title,
+    reviews.category,
+    reviews.designer,
+    reviews.owner,
+    reviews.review_body,
+    reviews.review_img_url,
+    reviews.created_at,
+    reviews.votes,
+    COUNT (comments.review_id):: INT AS comment_count
+    FROM reviews
+    LEFT JOIN comments ON reviews.review_id = comments.review_id`;
+  // let queryValue = [];
+  if (category) {
+    queryStr += ` WHERE reviews.category = $1 ORDER BY ${sort_by} ${order}`;
+    return db
+      .query(queryStr, [category])
+      .then((result) => {
+        return result.rows;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  queryStr += ` GROUP BY reviews.review_id ORDER BY reviews.created_at DESC;`;
+  return db.query(queryStr).then((result) => {
+    return result.rows;
+  });
+};
+
+module.exports = { selectReviewsById, updateReviewsById, selectReviews };
